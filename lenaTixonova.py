d0d296da253303b9f4eeb043fb602db4ea1ab0ce
opexpy05 <103304445+Opexpy@users.noreply.github.com>
2026-02-06 22:47:46 +0300

    Add files via upload

--- lenaTixonova.py
+++ lenaTixonova.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 # Конфигурация ключей
 TELEGRAM_TOKEN = "8361115667:AAH3V1LTbPESW4JyCIE5Mrr7KXJFKYGoWgE"
-MISTRAL_API_KEY = "hK3WmFALFcUl7neQTZzMXR7Q4IJ6HDbZ"
+MISTRAL_API_KEY = "CPNaA0wykAaEMBXWrVpk4qN9TmiGqOYG"
 
 # Проверка ключей перед инициализацией
 logger.info(f"TELEGRAM_TOKEN установлен: {bool(TELEGRAM_TOKEN and TELEGRAM_TOKEN != 'ВАШ_TELEGRAM_BOT_TOKEN')}")
